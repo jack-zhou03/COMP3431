c891d2b51d11a8d4f6183e01f8e4888432a99436
jack-zhou03 <88605396+jack-zhou03@users.noreply.github.com>
2025-11-20 10:37:15 +1100

    final working version in sim

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -22,107 +22,104 @@ def main() -> None:
     # Security route, probably read in from a file for a real application
     # from either a map or drive and repeat.
     security_route = [
-        # [-0.06242923379727182,-3.10064952800541],
-        # [-0.08372162803232902,-1.0074568385397238],
-        # [2.9708988171937776,0.13127214449794022],
+        [-0.06242923379727182,-3.10064952800541],
+        [-0.08372162803232902,-1.0074568385397238],
+        [2.9708988171937776,0.13127214449794022],
         [0.7699593453880917,-2.9495048140375753],
-        [0.0,-0.1]
     ]
 
-    # Set our demo's initial pose
-    # initial_pose = PoseStamped()
-    # initial_pose.header.frame_id = 'map'
-    # initial_pose.header.stamp = navigator.get_clock().now().to_msg()
-    # initial_pose.pose.position.x = 0.0
-    # initial_pose.pose.position.y = 0.0
-    # initial_pose.pose.orientation.z = 0.0
-    # initial_pose.pose.orientation.w = 1.0
-    # navigator.setInitialPose(initial_pose)
+    #Set our demo's initial pose
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+    initial_pose.pose.position.x = 0.0
+    initial_pose.pose.position.y = 0.0
+    initial_pose.pose.orientation.z = 0.0
+    initial_pose.pose.orientation.w = 1.0
+    navigator.setInitialPose(initial_pose)
 
     # Wait for navigation to fully activate
     navigator.waitUntilNav2Active()
 
-    # Do security route until dead
-    while rclpy.ok():
-        # Send our route
-        route_poses = []
-        pose = PoseStamped()
-        pose.header.frame_id = 'map'
-        pose.header.stamp = navigator.get_clock().now().to_msg()
-        pose.pose.orientation.w = 1.0
-        for pt in security_route:
-            pose.pose.position.x = pt[0]
-            pose.pose.position.y = pt[1]
-            route_poses.append(deepcopy(pose))
-        go_through_poses_task = navigator.goThroughPoses(route_poses)
+    # Send our route
+    route_poses = []
+    pose = PoseStamped()
+    pose.header.frame_id = 'map'
+    pose.header.stamp = navigator.get_clock().now().to_msg()
+    pose.pose.orientation.w = 1.0
+    for pt in security_route:
+        pose.pose.position.x = pt[0]
+        pose.pose.position.y = pt[1]
+        route_poses.append(deepcopy(pose))
+    go_through_poses_task = navigator.goThroughPoses(route_poses)
 
-        # Do something during our route (e.x. AI detection on camera images for anomalies)
-        # Simply print ETA for the demonstration
-        i = 0
-        while not navigator.isTaskComplete():
-            i += 1
-            feedback = navigator.getFeedback()
-            if feedback and i % 5 == 0:
-                print(
-                    'Estimated time to complete current route: '
-                    + '{:.0f}'.format(
-                        Duration.from_msg(feedback.estimated_time_remaining).nanoseconds
-                        / 1e9
-                    )
-                    + ' seconds.'
+    # Do something during our route (e.x. AI detection on camera images for anomalies)
+    # Simply print ETA for the demonstration
+    i = 0
+    while not navigator.isTaskComplete():
+        i += 1
+        feedback = navigator.getFeedback()
+        if feedback and i % 5 == 0:
+            print(
+                'Estimated time to complete current route: '
+                + '{:.0f}'.format(
+                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds
+                    / 1e9
                 )
+                + ' seconds.'
+            )
 
-                # Some failure mode, must stop since the robot is clearly stuck
-                if Duration.from_msg(feedback.navigation_time) > Duration(
-                    seconds=180.0
-                ):
-                    print('Navigation has exceeded timeout of 180s, canceling request.')
-                    navigator.cancelTask()
+            # Some failure mode, must stop since the robot is clearly stuck
+            if Duration.from_msg(feedback.navigation_time) > Duration(
+                seconds=180.0
+            ):
+                print('Navigation has exceeded timeout of 180s, canceling request.')
+                navigator.cancelTask()
 
 
-        result = navigator.getResult()
-        if result == TaskResult.SUCCEEDED:
-            print('Route complete! Restarting...')
-        elif result == TaskResult.CANCELED:
-            print('Security route was canceled, exiting.')
-            exit(1)
-        elif result == TaskResult.FAILED:
-            (error_code, error_msg) = navigator.getTaskError()
-            print(f'Security route failed!:{error_code}:{error_msg}')
-            print('Restarting from other side...')
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Route complete! Restarting...')
+    elif result == TaskResult.CANCELED:
+        print('Security route was canceled, exiting.')
+        exit(1)
+    elif result == TaskResult.FAILED:
+        (error_code, error_msg) = navigator.getTaskError()
+        print(f'Security route failed!:{error_code}:{error_msg}')
+        print('Restarting from other side...')
 
-        # # go back to the start 
+    # go back to the start 
 
-        # start_pose = PoseStamped()
-        # start_pose.header.frame_id = 'map'
-        # start_pose.header.stamp = navigator.get_clock().now().to_msg()
-        # start_pose.pose.position.x = 0.0
-        # start_pose.pose.position.y = 0.0
-        # start_pose.pose.orientation.w = 1.0
-        # navigator.goToPose(start_pose)
-        # while not navigator.isTaskComplete():
-        #     i+=1
-        #     feedback = navigator.getFeedback()
-        #     if feedback and i % 5 == 0:
-        #         print(
-        #             'Estimated time to return to start: '
-        #             + '{:.0f}'.format(
-        #                 Duration.from_msg(feedback.estimated_time_remaining).nanoseconds
-        #                 / 1e9
-        #             )
-        #             + ' seconds.'
-        #         )
-        
-        # result = navigator.getResult()
-        # if result == TaskResult.SUCCEEDED:
-        #     print('Returned to start position, restarting route...')
-        # elif result == TaskResult.CANCELED:
-        #     print('Return to start was canceled, exiting.')
-        #     exit(1)
-        # elif result == TaskResult.FAILED:
-        #     (error_code, error_msg) = navigator.getTaskError()
-        #     print(f'Return to start failed!:{error_code}:{error_msg}')
-        #     print('Exiting.')
+    start_pose = PoseStamped()
+    start_pose.header.frame_id = 'map'
+    start_pose.header.stamp = navigator.get_clock().now().to_msg()
+    start_pose.pose.position.x = 0.0
+    start_pose.pose.position.y = 0.0
+    start_pose.pose.orientation.w = 1.0
+    navigator.goToPose(start_pose)
+    while not navigator.isTaskComplete():
+        i+=1
+        feedback = navigator.getFeedback()
+        if feedback and i % 5 == 0:
+            print(
+                'Estimated time to return to start: '
+                + '{:.0f}'.format(
+                    Duration.from_msg(feedback.estimated_time_remaining).nanoseconds
+                    / 1e9
+                )
+                + ' seconds.'
+            )
+    
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Returned to start position, restarting route...')
+    elif result == TaskResult.CANCELED:
+        print('Return to start was canceled, exiting.')
+        exit(1)
+    elif result == TaskResult.FAILED:
+        (error_code, error_msg) = navigator.getTaskError()
+        print(f'Return to start failed!:{error_code}:{error_msg}')
+        print('Exiting.')
 
     exit(0)
 
